c9b4762e18fb42617f4d7521139e750daa689f14
Jerry Wang <jerryappleid761208@gmail.com>
2022-11-08 11:00:03 -0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
